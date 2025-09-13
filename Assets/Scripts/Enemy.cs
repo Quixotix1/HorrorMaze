e0e09219535badc7152b873be4abdff4001c6ec7
Quixotix1 <joshuageelenfamily@gmail.com>
2025-09-12 23:19:09 -0400

    Added Enemy Hitboxes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    GameObject player;
+    bool isMoving;
+    [HideInInspector] public float visibility = 0f;
+
     public void Teleport(Transform player)
     {
         transform.position = player.position;
@@ -13,5 +17,32 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+
+        if (isMoving)
+        {
+            Debug.Log(visibility);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 2f * Time.deltaTime);
+        }
+
+        if (visibility <= 0f)
+        {
+            isMoving = false;
+            gameObject.SetActive(false);
+        }
+
+        if (visibility > 1f) visibility = 1f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            isMoving = true;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -45,14 +45,27 @@ public class Player : MonoBehaviour
                 exit.SetActive(true);
 
                 canUseTorch = false;
+                torch = false;
             }
 
             enemy.gameObject.SetActive(true);
             enemy.Teleport(transform);
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (pellets != 4 && !torch) enemy.visibility -= 0.5f * Time.deltaTime;
+            else enemy.visibility += Time.deltaTime;
+        }
 
         if (other.CompareTag("Exit"))
         {
+            Debug.Log("escaping");
+
             if (Input.GetKeyDown(KeyCode.E))
             {
                 SceneManager.LoadScene("WinScene");
